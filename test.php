c3a43626434e81792cbc330621e1facd4bb0fd45
sketch-sina <74036845+sketch-sina@users.noreply.github.com>
2024-01-29 11:50:12 +0330

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+{
+  "banner": "Example Banner",
+  "dns": [
+    {
+      "dns1": "8.8.8.8",
+      "dns2": "8.8.4.4",
+      "name": "Google DNS"
+    },
+    {
+      "dns1": "208.67.222.222",
+      "dns2": "208.67.220.220",
+      "name": "OpenDNS"
+    }
+  ]
+}
